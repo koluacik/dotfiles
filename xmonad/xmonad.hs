b28e53d6d29423feb423a2ebbbad1f9120a6adbb
koluacik <koluacik@disroot.org>
2020-05-28 23:09:04 +0300

    fix indent

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,9 @@
-import System.IO
-import System.Process
-import XMonad
-import XMonad.Actions.NoBorders
-import XMonad.Config.Desktop
+import           System.IO
+import           System.Process
 
+import           XMonad
+import           XMonad.Actions.NoBorders
+import           XMonad.Config.Desktop
 import           XMonad.Hooks.DynamicBars
 import           XMonad.Hooks.DynamicLog
 import qualified XMonad.Hooks.EwmhDesktops   as X.H.E
@@ -12,21 +12,21 @@ import           XMonad.Hooks.ManageDocks
 import           XMonad.Hooks.ManageHelpers
 import           XMonad.Hooks.Place
 
-import XMonad.Layout.Fullscreen
-import XMonad.Layout.NoBorders
+import           XMonad.Layout.Fullscreen
+import           XMonad.Layout.NoBorders
 
-import qualified XMonad.StackSet as W
+import qualified XMonad.StackSet             as W
 
-import XMonad.Util.EZConfig
-import XMonad.Util.Run
+import           XMonad.Util.EZConfig
+import           XMonad.Util.Run
 
-main = xmonad $ fullscreenSupport $ X.H.E.ewmh $ 
+main = xmonad $ fullscreenSupport $ X.H.E.ewmh $
     myConfig' `additionalKeys` myKeys `additionalMouseBindings` myButtons `additionalKeysP` myKeys'
 
 myConfig' =
-  docks $
-    defaultConfig
-      { terminal = myTerminal,
+    docks $
+        defaultConfig
+            { terminal = myTerminal,
         -- hooks
         layoutHook = myLayoutHook,
         manageHook = myManageHook,
@@ -34,7 +34,7 @@ myConfig' =
         logHook = myLogHook,
         startupHook = myStartupHook,
         modMask = myModMask
-      }
+            }
 
 myTerminal = "kitty"
 
@@ -47,11 +47,11 @@ myLayout = (avoidStruts $ Tall 1 (3/100) (1/2)) ||| (avoidStruts $ Mirror $ Tall
 myManageHook =
     composeAll
     [ insertPosition Below Newer
-    , manageDocks
-    , manageHook defaultConfig
-    , isDialog --> doFloat
-    , className =? "Sxiv" --> doFloat
-    , className =? "feh" --> doFloat
+        , manageDocks
+        , manageHook defaultConfig
+        , isDialog --> doFloat
+        , className =? "Sxiv" --> doFloat
+        , className =? "feh" --> doFloat
     ]
 
 myStartupHook = dynStatusBarStartup startBar killBars
@@ -62,7 +62,6 @@ myLogHook = myMultiPP
 
 startBar :: DynamicStatusBar --ScreenId -> IO Handle
 startBar (S id) = spawnPipe $ "xmobar -x " ++ show id
-
 killBars :: DynamicStatusBarCleanup -- IO () ()
 killBars = return ()
 
@@ -70,8 +69,8 @@ myMultiPP :: X ()
 myMultiPP = multiPP focusedPP unfocusedPP
   where
     focusedPP =
-      xmobarPP
-        { ppCurrent = xmobarColor "black" "" . myWrap,
+        xmobarPP
+            { ppCurrent = xmobarColor "black" "" . myWrap,
           --ppTitle = xmobarColor "black" "" . shorten 40,
           ppTitle = const "",
           ppVisible = const "",
@@ -80,10 +79,10 @@ myMultiPP = multiPP focusedPP unfocusedPP
           ppHidden = xmobarColor "black" "" . wrap "<" ">",
           ppUrgent = xmobarColor "red" "" . myWrap,
           ppSep = " | "
-        }
+            }
     unfocusedPP =
-      xmobarPP
-        { ppCurrent = xmobarColor "black" "" . myWrap,
+        xmobarPP
+            { ppCurrent = xmobarColor "black" "" . myWrap,
           --ppTitle = xmobarColor "black" "" . shorten 40,
           ppTitle = const "",
           ppVisible = const "",
@@ -91,26 +90,26 @@ myMultiPP = multiPP focusedPP unfocusedPP
           ppHidden = xmobarColor "black" "" . wrap "<" ">",
           ppUrgent = xmobarColor "black" "" . myWrap,
           ppSep = " | "
-        }
+            }
 
 myWrap :: String -> String
 myWrap = wrap "[" "]"
 
 myKeys =
     [ ((myModMask .|. controlMask, xK_space), liftIO switchXkbLayout)
-    , ((myModMask, xK_bracketleft), spawn "sleep 1.2; scrotfeh")
-    , ((myModMask, xK_Print), spawn "scrotfeh select")
-    , ((myModMask .|. controlMask, xK_p), spawn "echo 'select\nright\nleft\nfull' | dmenu -p 'screenshot mode' | xargs scrotfeh")
+        , ((myModMask, xK_bracketleft), spawn "sleep 1.2; scrotfeh")
+        , ((myModMask, xK_Print), spawn "scrotfeh select")
+        , ((myModMask .|. controlMask, xK_p), spawn "echo 'select\nright\nleft\nfull' | dmenu -p 'screenshot mode' | xargs scrotfeh")
     ]
 
 myKeys' =
     [ ("<XF86AudioPlay>", spawn "playerctl play-pause")
-    , ("<XF86AudioPrev>", spawn "playerctl previous")
-    , ("<XF86AudioNext>", spawn "playerctl next")
-    , ("<XF86AudioRaiseVolume>", spawn "amixer -D pulse sset Master 5%+")
-    , ("<XF86AudioLowerVolume>", spawn "amixer -D pulse sset Master 5%-")
-    , ("<XF86MonBrightnessUp>", spawn "brightnessctl s +10%")
-    , ("<XF86MonBrightnessDown>", spawn "brightnessctl s -10%")
+        , ("<XF86AudioPrev>", spawn "playerctl previous")
+        , ("<XF86AudioNext>", spawn "playerctl next")
+        , ("<XF86AudioRaiseVolume>", spawn "amixer -D pulse sset Master 5%+")
+        , ("<XF86AudioLowerVolume>", spawn "amixer -D pulse sset Master 5%-")
+        , ("<XF86MonBrightnessUp>", spawn "brightnessctl s +10%")
+        , ("<XF86MonBrightnessDown>", spawn "brightnessctl s -10%")
     ]
 
 myButtons =
@@ -120,15 +119,15 @@ myButtons =
 
 switchXkbLayout :: IO ()
 switchXkbLayout = do
-  layout <- getXkbLayout
-  let args = "-option altwin:swap_lalt_lwin,lv3:ralt_switch_multikey"
-  let capsLockCommand = "xmodmap -e 'keycode 105 = Caps_Lock'"
-  case layout of
-    "us" -> spawn $ "setxkbmap tr " ++ args ++ "; " ++ capsLockCommand
-    "tr" -> spawn $ "setxkbmap us " ++ args ++ "; " ++ capsLockCommand
+    layout <- getXkbLayout
+    let args = "-option altwin:swap_lalt_lwin,lv3:ralt_switch_multikey"
+    let capsLockCommand = "xmodmap -e 'keycode 105 = Caps_Lock'"
+    case layout of
+        "us" -> spawn $ "setxkbmap tr " ++ args ++ "; " ++ capsLockCommand
+        "tr" -> spawn $ "setxkbmap us " ++ args ++ "; " ++ capsLockCommand
 
 getXkbLayout :: IO String
 getXkbLayout = do
-  let processString = "setxkbmap -query | grep layout | tail -c 3"
-  (_, Just hout, _, _) <- createProcess (shell processString) {std_out = CreatePipe}
-  hGetLine hout
+    let processString = "setxkbmap -query | grep layout | tail -c 3"
+    (_, Just hout, _, _) <- createProcess (shell processString) {std_out = CreatePipe}
+    hGetLine hout
