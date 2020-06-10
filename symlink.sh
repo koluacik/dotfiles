50ebd1f2a8ad0efd3a65ec5dddaec223257b9abd
Deniz Koluaçık <koluacik@disroot.org>
2020-06-10 22:16:56 +0300

    add xmonad build file and startup xcursor

--- symlink.sh
+++ symlink.sh
@@ -8,6 +8,7 @@ ln -sf $PWD/nvim $HOME/.config/
 ln -sf $PWD/ranger $HOME/.config/
 ln -sf $PWD/stylish-haskell $HOME/.config/
 ln -sf $PWD/xmonad/xmonad.hs $HOME/.xmonad/
+ln -sf $PWD/xmonad/build $HOME/.xmonad/
 ln -sf $PWD/xmonad/xmonad.start $HOME/.xmonad/
 ln -sf $PWD/xmobar/xmobarrc $HOME/.xmobarrc
 
